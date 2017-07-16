c4fc6cd05bd0aaa49fc57a302876c60fbb167b82
Anartz Mugika <mugan86@gmail.com>
2017-07-16 17:33:38 +0200

    Finish Chapter 11

--- app/src/main/java/amldev/kotlinfordevelopers/domain/model/DomainClasses.kt
+++ app/src/main/java/amldev/kotlinfordevelopers/domain/model/DomainClasses.kt
@@ -3,5 +3,8 @@ package amldev.kotlinfordevelopers.domain.model
 /**
  * Created by anartzmugika on 15/7/17.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>){
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
+}
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
