0fa37713ada55e5e5142cd60bfea84347624b41d
Anartz Muxika <mugan86@gmail.com>
2017-08-15 10:33:04 +0200

    Add ForecastDbHelper first version

---
+++ app/src/main/java/amldev/kotlinfordevelopers/data/db/ForecastDbHelper.kt
@@ -0,0 +1,40 @@
+package amldev.kotlinfordevelopers.data.db
+
+import amldev.kotlinfordevelopers.ui.App
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+/**
+ * Created by Anartz Mugika on 15/08/2017.
+ */
+class ForecastDbHelper() : ManagedSQLiteOpenHelper(App.instance,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(CityForecastTable.NAME, true)
+        db.dropTable(DayForecastTable.NAME, true)
+        onCreate(db)
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(CityForecastTable.NAME, true,
+                CityForecastTable.ID to INTEGER + PRIMARY_KEY,
+                CityForecastTable.CITY to TEXT,
+                CityForecastTable.COUNTRY to TEXT)
+
+        db.createTable(DayForecastTable.NAME, true,
+                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.DATE to INTEGER,
+                DayForecastTable.DESCRIPTION to TEXT,
+                DayForecastTable.HIGH to INTEGER,
+                DayForecastTable.LOW to INTEGER,
+                DayForecastTable.ICON_URL to TEXT,
+                DayForecastTable.CITY_ID to INTEGER)    }
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance by lazy { ForecastDbHelper() }
+    }
+
+}
