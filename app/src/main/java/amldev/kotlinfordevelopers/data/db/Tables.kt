f1aae9437adaf3e954eba1da9572b6b1b1ec7805
Anartz Muxika <mugan86@gmail.com>
2017-08-15 10:16:14 +0200

    Add Forecast Database tables

---
+++ app/src/main/java/amldev/kotlinfordevelopers/data/db/Tables.kt
@@ -0,0 +1,22 @@
+package amldev.kotlinfordevelopers.data.db
+
+/**
+ * Created by Anartz Mugika on 15/08/2017.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
