620d700b7461f94fbdd499e72a24a94c89ea4b60
Anartz Muxika <mugan86@gmail.com>
2017-08-16 21:53:08 +0200

    Pass context

--- app/src/main/java/amldev/kotlinfordevelopers/domain/commands/RequestForecastCommand.kt
+++ app/src/main/java/amldev/kotlinfordevelopers/domain/commands/RequestForecastCommand.kt
@@ -3,14 +3,15 @@ package amldev.kotlinfordevelopers.domain.commands
 import amldev.kotlinfordevelopers.data.ForecastRequest
 import amldev.kotlinfordevelopers.domain.mappers.ForecastDataMapper
 import amldev.kotlinfordevelopers.domain.model.ForecastList
+import android.content.Context
 
-/**
+/*******************************************************************
  * Created by anartzmugika on 15/7/17.
  */
-class RequestForecastCommand(private val location: String, private val type: Int = 1, private val days: Int = 7) : Command<ForecastList> {
+class RequestForecastCommand(private val location: String, private val type: Int = 1, private val days: Int = 7, val context : Context) : Command<ForecastList> {
     //Sobreescribimos el comando execute de la interfaz Command
     override fun execute(): ForecastList {
-        val forecastRequest = ForecastRequest(location, type, days)
+        val forecastRequest = ForecastRequest(location, type, days, context)
         if(type == 2) return ForecastDataMapper().convertFromDataHoursModel(forecastRequest.executeNextHours())
         return ForecastDataMapper().convertFromDataDailyModel(forecastRequest.executeNextDays())
     }
