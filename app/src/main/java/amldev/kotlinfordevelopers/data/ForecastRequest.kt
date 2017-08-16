4bccb229daf74d4be5ad9c4472bde8c1ae63b476
Anartz Muxika <mugan86@gmail.com>
2017-08-16 21:51:27 +0200

    Load data without internet

--- app/src/main/java/amldev/kotlinfordevelopers/data/ForecastRequest.kt
+++ app/src/main/java/amldev/kotlinfordevelopers/data/ForecastRequest.kt
@@ -1,6 +1,10 @@
 package amldev.kotlinfordevelopers.data
 
+import amldev.kotlinfordevelopers.utils.isNetworkConnected
+import android.content.Context
 import com.google.gson.Gson
+import com.google.gson.JsonParser
+import java.io.InputStreamReader
 import java.net.URL
 
 /***********************************************************************************************************************
@@ -12,7 +16,7 @@ import java.net.URL
  * donde se define la forma que tendrá la información al final del proceso de la petición
  ***********************************************************************************************************************/
 
-class ForecastRequest(val location: String, val type: Int = 1, val days: Int = 7) {
+class ForecastRequest(val location: String, val type: Int = 1, val days: Int = 7, val context: Context) {
 
     companion object {
 
@@ -36,12 +40,16 @@ class ForecastRequest(val location: String, val type: Int = 1, val days: Int = 7
 
     fun executeNextDays(): ForecastNextDaysResult {
         //Obtain forecastJSONStr and pass to convert in ForecastResult
-        return Gson().fromJson(URL(this.getUseUrl()).readText(), ForecastNextDaysResult::class.java)
+        if (isNetworkConnected(context)) return Gson().fromJson(URL(this.getUseUrl()).readText(), ForecastNextDaysResult::class.java)
+        //Without Internet, //Obtain forecastJSONStr and pass to convert in ForecastResult from json assets
+        return Gson().fromJson(getJSONResource(context, "next_days"), ForecastNextDaysResult::class.java)
     }
 
     fun executeNextHours(): ForecastNextHoursResult {
         //Obtain forecastJSONStr and pass to convert in ForecastResult
-        return Gson().fromJson(URL(this.getUseUrl()).readText(), ForecastNextHoursResult::class.java)
+        if (isNetworkConnected(context)) return Gson().fromJson(URL(this.getUseUrl()).readText(), ForecastNextHoursResult::class.java)
+        //Without Internet, //Obtain forecastJSONStr and pass to convert in ForecastResult from json assets
+        return Gson().fromJson(getJSONResource(context, "next_hours"), ForecastNextHoursResult::class.java)
     }
 
     // Create Use URL in request depending type, zipcode and other properties
@@ -57,6 +65,18 @@ class ForecastRequest(val location: String, val type: Int = 1, val days: Int = 7
             else return getNextDaysForecastURL(list[0], list[1], days, "en")
         }
         return getNextHoursForecastURL(list[0], list[1], "en")
+    }
+
+    fun getJSONResource(context: Context, name: String): String? {
+        try {
+            context.getAssets().open("$name.json").use({ `is` ->
+                val parser = JsonParser()
+                return parser.parse(InputStreamReader(`is`)).toString()
+            })
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
     }
 
 }
